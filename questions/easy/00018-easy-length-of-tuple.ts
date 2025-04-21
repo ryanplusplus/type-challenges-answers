dd5f449c7c8cf52b16757315f54852f3f5ab0653
Ryan Hartlage <ryanplusplus@gmail.com>
2025-04-21 19:29:42 -0400

    That was refreshingly easy

--- questions/easy/00018-easy-length-of-tuple.ts
+++ questions/easy/00018-easy-length-of-tuple.ts
@@ -22,13 +22,19 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Length<T> = any
+type Length<T extends readonly unknown[]> = T['length'];
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from '@type-challenges/utils';
 
-const tesla = ['tesla', 'model 3', 'model X', 'model Y'] as const
-const spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT'] as const
+const tesla = ['tesla', 'model 3', 'model X', 'model Y'] as const;
+const spaceX = [
+  'FALCON 9',
+  'FALCON HEAVY',
+  'DRAGON',
+  'STARSHIP',
+  'HUMAN SPACEFLIGHT',
+] as const;
 
 type cases = [
   Expect<Equal<Length<typeof tesla>, 4>>,
@@ -37,7 +43,7 @@ type cases = [
   Length<5>,
   // @ts-expect-error
   Length<'hello world'>,
-]
+];
 
 /* _____________ Further Steps _____________ */
 /*
