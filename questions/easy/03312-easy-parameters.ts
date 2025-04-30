24c9ee91ea2715e30b7ad6dd6b194a4d7a185baf
Ryan Hartlage <ryanplusplus@gmail.com>
2025-04-30 19:52:45 -0400

    Easy ones are done

--- questions/easy/03312-easy-parameters.ts
+++ questions/easy/03312-easy-parameters.ts
@@ -20,10 +20,10 @@
 
 /* _____________ Your Code Here _____________ */
 
-type MyParameters<T extends (...args: any[]) => any> = any
+type MyParameters<T> = T extends (...args: infer Args) => unknown ? Args : never;
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from '@type-challenges/utils';
 
 function foo(arg1: string, arg2: number): void {}
 function bar(arg1: boolean, arg2: { a: 'A' }): void {}
@@ -33,7 +33,7 @@ type cases = [
   Expect<Equal<MyParameters<typeof foo>, [string, number]>>,
   Expect<Equal<MyParameters<typeof bar>, [boolean, { a: 'A' }]>>,
   Expect<Equal<MyParameters<typeof baz>, []>>,
-]
+];
 
 /* _____________ Further Steps _____________ */
 /*
