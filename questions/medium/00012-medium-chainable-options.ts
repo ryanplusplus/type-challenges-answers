8a3152373abada3b0df8c7be3d317a6c56b629ee
Ryan Hartlage <ryanplusplus@gmail.com>
2025-05-03 12:02:21 -0400

    Wild

--- questions/medium/00012-medium-chainable-options.ts
+++ questions/medium/00012-medium-chainable-options.ts
@@ -39,55 +39,58 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Chainable = {
-  option(key: string, value: any): any
-  get(): any
-}
+type Chainable<T = object> = {
+  option<K extends PropertyKey, V>(
+    key: K extends keyof T ? never : K,
+    value: V,
+  ): Chainable<Omit<T, K> & Record<K, V>>;
+  get(): T;
+};
 
 /* _____________ Test Cases _____________ */
-import type { Alike, Expect } from '@type-challenges/utils'
+import type { Alike, Expect } from '@type-challenges/utils';
 
-declare const a: Chainable
+declare const a: Chainable;
 
 const result1 = a
   .option('foo', 123)
   .option('bar', { value: 'Hello World' })
   .option('name', 'type-challenges')
-  .get()
+  .get();
 
 const result2 = a
   .option('name', 'another name')
   // @ts-expect-error
   .option('name', 'last name')
-  .get()
+  .get();
 
 const result3 = a
   .option('name', 'another name')
   // @ts-expect-error
   .option('name', 123)
-  .get()
+  .get();
 
 type cases = [
   Expect<Alike<typeof result1, Expected1>>,
   Expect<Alike<typeof result2, Expected2>>,
   Expect<Alike<typeof result3, Expected3>>,
-]
+];
 
 type Expected1 = {
-  foo: number
+  foo: number;
   bar: {
-    value: string
-  }
-  name: string
-}
+    value: string;
+  };
+  name: string;
+};
 
 type Expected2 = {
-  name: string
-}
+  name: string;
+};
 
 type Expected3 = {
-  name: number
-}
+  name: number;
+};
 
 /* _____________ Further Steps _____________ */
 /*
