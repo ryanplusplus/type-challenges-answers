0b7cdc46742365daf6dc2cab65f912fb85345abc
Ryan Hartlage <ryanplusplus@gmail.com>
2025-05-03 08:29:49 -0400

    ggez

--- questions/medium/00010-medium-tuple-to-union.ts
+++ questions/medium/00010-medium-tuple-to-union.ts
@@ -20,15 +20,15 @@
 
 /* _____________ Your Code Here _____________ */
 
-type TupleToUnion<T> = any
+type TupleToUnion<T> = T extends [infer First, ...infer Rest] ? First | TupleToUnion<Rest> : never;
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from '@type-challenges/utils';
 
 type cases = [
   Expect<Equal<TupleToUnion<[123, '456', true]>, 123 | '456' | true>>,
   Expect<Equal<TupleToUnion<[123]>, 123>>,
-]
+];
 
 /* _____________ Further Steps _____________ */
 /*
